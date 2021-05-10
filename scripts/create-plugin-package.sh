2ad86d2f121c42b4843e97d775b198f49e9a76ea
loso3000 <51484662+loso3000@users.noreply.github.com>
2021-05-10 13:54:30 +0800

    app

--- scripts/create-plugin-package.sh
+++ scripts/create-plugin-package.sh
@@ -2,7 +2,7 @@ cd openwrt/bin/targets/*/*
 mkdir ssrp
 
 # plugin list in ../../../packages/mipsel_24kc
-pkglist="base/shadowsocksr-libev-alt_*.ipk base/pdnsd-alt_*.ipk base/microsocks_*.ipk base/dns2socks_*.ipk base/shadowsocksr-libev-ssr-local_*.ipk base/simple-obfs_*.ipk base/tcping_*.ipk base/v2ray-plugin_*.ipk base/xray_*.ipk base/trojan_*.ipk base/ipt2socks_*.ipk base/redsocks2_*.ipk base/luci-app-ssr-plus_*.ipk base/luci-i18n-ssr-plus-zh-cn_*.ipk luci/luci-compat_*.ipk base/xray-core_*.ipk gli_pub/shadowsocks-libev-ss-*.ipk"
+pkglist="base/luci-i18n-smartdns*.ipk base/luci-i18n-advanced*.ipk base/luci-i18n-autotimeset*.ipk base/luci-i18n-netspeedtest*.ipk base/luci-i18n-openclash*.ipk base/smartdns*.ipk base/luci-app-smartdns*.ipk base/luci-app-advanced*.ipk base/luci-app-autotimeset*.ipk base/luci-app-netspeedtest*.ipk base/luci-app-openclash*.ipk base/shadowsocksr-libev-alt_*.ipk base/pdnsd-alt_*.ipk base/microsocks_*.ipk base/dns2socks_*.ipk base/shadowsocksr-libev-ssr-local_*.ipk base/simple-obfs_*.ipk base/tcping_*.ipk base/v2ray-plugin_*.ipk base/xray_*.ipk base/trojan_*.ipk base/ipt2socks_*.ipk base/redsocks2_*.ipk base/luci-app-ssr-plus_*.ipk base/luci-i18n-ssr-plus-zh-cn_*.ipk luci/luci-compat_*.ipk base/xray-core_*.ipk gli_pub/shadowsocks-libev-ss-*.ipk"
 
 for pkg in $pkglist
 do
@@ -30,6 +30,7 @@ opkg install ./tcping_*.ipk
 opkg install ./v2ray-plugin_*.ipk 
 opkg install ./ipt2socks_*.ipk
 opkg install ./xray-core_*.ipk
+opkg install ./trojan_*.ipk
 opkg install ./shadowsocks-libev-ss-local_*.ipk
 opkg install ./shadowsocks-libev-ss-server_*.ipk
 opkg install ./shadowsocks-libev-ss-redir_*.ipk
