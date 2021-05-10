a3778eea5171514ed27b90c16ac66e91dc8896ce
loso3000 <51484662+loso3000@users.noreply.github.com>
2021-05-10 13:45:49 +0800

    redsocks2

--- scripts/customize-feeds-gl.sh
+++ scripts/customize-feeds-gl.sh
@@ -33,15 +33,12 @@ rm -rf /workdir/openwrt/feeds/packages/net/wget
 cp -rp /workdir/lede/package/lean/wget /workdir/openwrt/feeds/packages/net/wget
 cp -rp /workdir/lede/package/lean/wget /workdir/openwrt/package/lean/wget
 
-
+cd /workdir/openwrt
 #add sirpdboy
 svn co https://github.com/sirpdboy/sirpdboy-package/trunk/luci-app-advanced ./package/new/luci-app-advanced
 svn co https://github.com/sirpdboy/sirpdboy-package/trunk/luci-app-netspeedtest ./package/new/luci-app-netspeedtest
 svn co https://github.com/sirpdboy/sirpdboy-package/trunk/luci-app-autotimeset ./package/new/luci-app-autotimeset
-svn co https://github.com/jerrykuku/luci-app-vssr/trunk/  package/diy/luci-app-vssr
-
-# add openclash
-svn co https://github.com/vernesong/OpenClash/trunk/luci-app-openclash package/diy/luci-app-openclash
+svn co https://github.com/jerrykuku/luci-app-vssr/trunk/  package/new/luci-app-vssr
 
 # Clone community packages to package/community
 mkdir package/community
@@ -55,10 +52,29 @@ git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall
 # Add gotop
 svn co https://github.com/project-openwrt/openwrt/trunk/package/ctcgfw/gotop
 
-# Add smartdns
-svn co https://github.com/pymumu/smartdns/trunk/package/openwrt ../smartdns
-svn co https://github.com/project-openwrt/openwrt/trunk/package/ntlf9t/luci-app-smartdns ../luci-app-smartdns
 
 # Add adbyby
 cp -r /workdir/lede/package/lean/adbyby ./
 cp -r /workdir/lede/package/lean/luci-app-adbyby-plus ./
+
+# Add openclash
+git clone --depth=1 -b master https://github.com/vernesong/OpenClash
+ARCH=mipsle-hardfloat
+cd /workdir/openwrt
+mkdir -p files/etc/openclash/core
+cd files/etc/openclash/core
+
+clash_main_url=$(curl -sL https://api.github.com/repos/vernesong/OpenClash/releases/tags/Clash | grep /clash-linux-$ARCH | sed 's/.*url\": \"//g' | sed 's/\"//g')
+#clash_tun_url=$(curl -sL https://api.github.com/repos/vernesong/OpenClash/releases/tags/TUN-Premium | grep /clash-linux-$ARCH | sed 's/.*url\": \"//g' | sed 's/\"//g')
+#clash_game_url=$(curl -sL https://api.github.com/repos/vernesong/OpenClash/releases/tags/TUN | grep /clash-linux-$ARCH | sed 's/.*url\": \"//g' | sed 's/\"//g')
+
+wget $clash_main_url && tar zxvf clash-linux-*.gz && rm -f clash-linux-*.gz
+#wget -qO- $clash_main_url | gunzip -c > clash
+#wget -qO- $clash_tun_url  | gunzip -c > clash_tun
+#wget -qO- $clash_game_url | tar xOvz > clash_game
+
+chmod +x clash*
+
+mkdir /workdir/openwrt/package/lean/redsocks2 
+cd /workdir/openwrt/package/lean/redsocks2 
+wget https://github.com/coolsnowwolf/lede/blob/master/package/lean/redsocks2/Makefile
